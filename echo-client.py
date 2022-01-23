8b1a0c5b00d23cc0147641efbdc21db79dcff7db
BuildTools <unconfigured@null.spigotmc.org>
2022-01-23 13:13:52 -0600

    continuous responses

--- echo-client.py
+++ echo-client.py
@@ -6,13 +6,14 @@ clear = lambda: os.system("cls")
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-x = 0
-while x > -1:
+#x = 0
+#while x > -1:
+while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         ini = str(input("Response: "))
         s.sendall(bytes(''+ini+'', "UTF-8"))
         data = s.recv(1024)
-    x -= 1
+#    x -= 1
 clear()
 print('Received: '+data.decode())
--- echo-server.py
+++ echo-server.py
@@ -1,17 +1,23 @@
 import socket
+import os
 
+clear = lambda: os.system("cls")
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(addr[0]+" sent "+data.decode())
-            conn.sendall(data)
+x = False
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            if not x:
+                print('Connected by', addr)
+                x = True
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(addr[0]+" sent "+data.decode())
+                conn.sendall(data)
